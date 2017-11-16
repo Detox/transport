df83228e1fac98569cbfda37cf2eda6fa79a53b2
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2017-11-16 20:58:09 +0200

    Continued hacking on custom `simple-peer` version: added support for sending routing data separately from DHT data using the same data channel

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@
  * @license   MIT License, see license.txt
  */
 (function(){
-  function Transport(webtorrentDht, ronion, jssha){
-    var webrtcSocket, simplePeer, x$, y$;
+  function Transport(webtorrentDht, ronion, jssha, asyncEventer){
+    var webrtcSocket, simplePeer, x$, y$, z$;
     webrtcSocket = webtorrentDht({
       bootstrap: []
     })._rpc.socket.socket;
@@ -44,8 +44,8 @@
       value: webrtcSocketDetox
     });
     /**
-     * We'll authenticate remove peers by requiring them to sign SDP by their DHT key as well as will send all of the data in chunks of fixed size after
-     * fixed time intervals
+     * We'll authenticate remove peers by requiring them to sign SDP by their DHT key
+     * TODO: ^ is not implemented yet
      *
      * @constructor
      *
@@ -60,6 +60,20 @@
     simplePeerDetox.prototype = Object.create(simplePeer.prototype);
     y$ = simplePeerDetox.prototype;
     /**
+     * Dirty hack to get `data` event and handle it the way we want
+     */
+    y$.emit = function(event, data){
+      if (event === 'data') {
+        if (data[0] === 1) {
+          simplePeer.prototype.emit.call(this, 'data', data.subarray(1));
+        } else {
+          simplePeer.prototype.emit.call(this, 'routing_data', data.subarray(1));
+        }
+      } else {
+        simplePeer.prototype.emit.apply(this, arguments);
+      }
+    };
+    /**
      * Data sending method that will be used by DHT
      *
      * @param {Buffer} data
@@ -67,9 +81,14 @@
     y$.send = function(data){
       this.real_send(data, true);
     };
-    y$.send_routing(function(data){
+    /**
+     * Data sending method that will be used by anonymous routing
+     *
+     * @param {Uint8Array} data
+     */
+    y$.send_routing_data = function(data){
       this.real_send(data, false);
-    });
+    };
     /**
      * Actual data sending method moved here
      *
@@ -77,7 +96,11 @@
      * @param {boolean}		for_dht	Whether data sent are for DHT or not
      */
     y$.real_send = function(data, for_dht){
-      simplePeer.prototype.send.call(this, data);
+      var x$, data_with_header;
+      x$ = data_with_header = new Uint8Array(data.length + 1);
+      x$.set([for_dht ? 1 : 0]);
+      x$.set(data, 1);
+      simplePeer.prototype.send.call(this, data_with_header);
     };
     Object.defineProperty(simplePeerDetox.prototype, 'constructor', {
       enumerable: false,
@@ -105,48 +128,58 @@
      * @return {DHT}
      */
     function DHT(node_id, bootstrap_nodes, ice_servers, bucket_size){
+      var socket, x$, this$ = this;
       bucket_size == null && (bucket_size = 2);
       if (!(this instanceof DHT)) {
         return new DHT(node_id, bootstrap_nodes, ice_servers, bucket_size);
       }
+      asyncEventer.call(this);
+      socket = webrtcSocketDetox({
+        simple_peer_constructor: simplePeerDetox,
+        simple_peer_opts: {
+          config: {
+            iceServers: ice_servers
+          }
+        }
+      });
+      x$ = socket;
+      x$.on('node_connected', function(id){
+        this$.fire('node_connected', id);
+      });
+      x$.on('node_disconnected', function(id){
+        this$.fire('node_disconnected', id);
+      });
       this._dht = new DHT({
         bootstrap: bootstrap_nodes,
         hash: sha3_256,
         k: bucket_size,
         nodeId: node_id,
-        socket: webrtcSocketDetox({
-          simple_peer_constructor: simplePeerDetox,
-          simple_peer_opts: {
-            config: {
-              iceServers: ice_servers
-            }
-          }
-        })
+        socket: socket
       });
     }
-    DHT.prototype = {
-      /**
-       * Start WebSocket server listening on specified ip:port, so that current node will be capable of acting as bootstrap node for other users
-       *
-       * @param {number}	port
-       * @param {string}	ip
-       */
-      start_bootstrap_node: function(port, ip){
-        this._dht.listen(port, ip);
-      }
-      /**
-       * @return {!string[]}
-       */,
-      get_bootstrap_nodes: function(){
-        return this._dht.toJSON().nodes;
-      }
-      /**
-       * @param {Function} callback
-       */,
-      'destroy': function(callback){
-        this._dht.destroy(callback);
-        delete this._dht;
-      }
+    DHT.prototype = Object.create(asyncEventer.prototype);
+    z$ = DHT.prototype;
+    /**
+     * Start WebSocket server listening on specified ip:port, so that current node will be capable of acting as bootstrap node for other users
+     *
+     * @param {number}	port
+     * @param {string}	ip
+     */
+    z$['start_bootstrap_node'] = function(port, ip){
+      this._dht.listen(port, ip);
+    };
+    /**
+     * @return {!string[]}
+     */
+    z$['get_bootstrap_nodes'] = function(){
+      return this._dht.toJSON().nodes;
+    };
+    /**
+     * @param {Function} callback
+     */
+    z$['destroy'] = function(callback){
+      this._dht.destroy(callback);
+      delete this._dht;
     };
     Object.defineProperty(DHT.prototype, 'constructor', {
       enumerable: false,
@@ -157,10 +190,10 @@
     };
   }
   if (typeof define === 'function' && define['amd']) {
-    define(['webtorrent-dht', 'ronion', 'jssha/src/sha3'], Transport);
+    define(['webtorrent-dht', 'ronion', 'jssha/src/sha3', 'async-eventer'], Transport);
   } else if (typeof exports === 'object') {
-    module.exports = Transport(require('webtorrent-dht'), require('ronion'), require('jssha/src/sha3'));
+    module.exports = Transport(require('webtorrent-dht'), require('ronion'), require('jssha/src/sha3'), require('async-eventer'));
   } else {
-    this['detox_transport'] = Transport(this['webtorrent_dht'], this['ronion'], this['jsSHA']);
+    this['detox_transport'] = Transport(this['webtorrent_dht'], this['ronion'], this['jsSHA'], this['async_eventer']);
   }
 }).call(this);
--- src/index.ls
+++ src/index.ls
@@ -4,7 +4,7 @@
  * @copyright Copyright (c) 2017, Nazar Mokrynskyi
  * @license   MIT License, see license.txt
  */
-function Transport (webtorrent-dht, ronion, jssha)
+function Transport (webtorrent-dht, ronion, jssha, async-eventer)
 	webrtc-socket	= webtorrent-dht({bootstrap: []})._rpc.socket.socket
 	# TODO: Dirty hack in order to not include simple-peer second time on frontend
 	simple-peer		= webrtc-socket._simple_peer_constructor
@@ -36,8 +36,8 @@ function Transport (webtorrent-dht, ronion, jssha)
 
 	Object.defineProperty(webrtc-socket-detox::, 'constructor', {enumerable: false, value: webrtc-socket-detox})
 	/**
-	 * We'll authenticate remove peers by requiring them to sign SDP by their DHT key as well as will send all of the data in chunks of fixed size after
-	 * fixed time intervals
+	 * We'll authenticate remove peers by requiring them to sign SDP by their DHT key
+	 * TODO: ^ is not implemented yet
 	 *
 	 * @constructor
 	 *
@@ -51,13 +51,29 @@ function Transport (webtorrent-dht, ronion, jssha)
 	simple-peer-detox:: = Object.create(simple-peer::)
 	simple-peer-detox::
 		/**
+		 * Dirty hack to get `data` event and handle it the way we want
+		 */
+		..emit = (event, data) !->
+			if event == 'data'
+				if data[0] == 1 # DHT data
+					simple-peer::emit.call(@, 'data', data.subarray(1))
+				else # Routing data
+					simple-peer::emit.call(@, 'routing_data', data.subarray(1))
+			else
+				simple-peer::emit.apply(@, &)
+		/**
 		 * Data sending method that will be used by DHT
 		 *
 		 * @param {Buffer} data
 		 */
 		..send = (data) !->
 			@real_send(data, true)
-		..send_routing (data) !->
+		/**
+		 * Data sending method that will be used by anonymous routing
+		 *
+		 * @param {Uint8Array} data
+		 */
+		..send_routing_data = (data) !->
 			@real_send(data, false)
 		/**
 		 * Actual data sending method moved here
@@ -66,8 +82,10 @@ function Transport (webtorrent-dht, ronion, jssha)
 		 * @param {boolean}		for_dht	Whether data sent are for DHT or not
 		 */
 		..real_send = (data, for_dht) !->
-			# TODO: Differentiate between DHT and anonymous routing here using `for_dht`
-			simple-peer::send.call(@, data)
+			data_with_header	= new Uint8Array(data.length + 1)
+				..set([if for_dht then 1 else 0])
+				..set(data, 1)
+			simple-peer::send.call(@, data_with_header)
 
 	Object.defineProperty(simple-peer-detox::, 'constructor', {enumerable: false, value: simple-peer-detox})
 	/**
@@ -92,37 +110,49 @@ function Transport (webtorrent-dht, ronion, jssha)
 	!function DHT (node_id, bootstrap_nodes, ice_servers, bucket_size = 2)
 		if !(@ instanceof DHT)
 			return new DHT(node_id, bootstrap_nodes, ice_servers, bucket_size)
-		@_dht	= new DHT(
-			bootstrap			: bootstrap_nodes
-			hash				: sha3_256
-			k					: bucket_size
-			nodeId				: node_id
-			socket				: webrtc-socket-detox(
-				simple_peer_constructor	: simple-peer-detox
-				simple_peer_opts		:
-					config	:
-						iceServers	: ice_servers
-			)
+		async-eventer.call(@)
+		socket	= webrtc-socket-detox(
+			simple_peer_constructor	: simple-peer-detox
+			simple_peer_opts		:
+				config	:
+					iceServers	: ice_servers
 		)
-	DHT::	=
+		socket
+			..on('node_connected', (id) !~>
+				@fire('node_connected', id)
+			)
+			..on('node_disconnected', (id) !~>
+				@fire('node_disconnected', id)
+			)
+		@_dht	= new DHT(
+			bootstrap	: bootstrap_nodes
+			hash		: sha3_256
+			k			: bucket_size
+			nodeId		: node_id
+			socket		: socket
+		)
+
+	DHT:: = Object.create(async-eventer::)
+	DHT::
 		/**
 		 * Start WebSocket server listening on specified ip:port, so that current node will be capable of acting as bootstrap node for other users
 		 *
 		 * @param {number}	port
 		 * @param {string}	ip
 		 */
-		start_bootstrap_node : (port, ip) !->
+		..'start_bootstrap_node' = (port, ip) !->
 			@_dht.listen(port, ip)
 		/**
 		 * @return {!string[]}
 		 */
-		get_bootstrap_nodes : ->
+		..'get_bootstrap_nodes' = ->
 			@_dht.toJSON().nodes
 		#TODO: more methods needed
 		/**
 		 * @param {Function} callback
 		 */
-		'destroy' : (callback) !->
+		..'destroy' = (callback) !->
+			# TODO: destroying should disconnect from any peers
 			@_dht.destroy(callback)
 			delete @_dht
 	Object.defineProperty(DHT::, 'constructor', {enumerable: false, value: DHT})
@@ -132,10 +162,10 @@ function Transport (webtorrent-dht, ronion, jssha)
 
 if typeof define == 'function' && define['amd']
 	# AMD
-	define(['webtorrent-dht', 'ronion', 'jssha/src/sha3'], Transport)
+	define(['webtorrent-dht', 'ronion', 'jssha/src/sha3', 'async-eventer'], Transport)
 else if typeof exports == 'object'
 	# CommonJS
-	module.exports = Transport(require('webtorrent-dht'), require('ronion'), require('jssha/src/sha3'))
+	module.exports = Transport(require('webtorrent-dht'), require('ronion'), require('jssha/src/sha3'), require('async-eventer'))
 else
 	# Browser globals
-	@'detox_transport' = Transport(@'webtorrent_dht', @'ronion', @'jsSHA')
+	@'detox_transport' = Transport(@'webtorrent_dht', @'ronion', @'jsSHA', @'async_eventer')
